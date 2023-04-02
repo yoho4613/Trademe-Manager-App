7da8da1777393d2fdb0edae84e8b4b80d478cbd4
박지호 <yoho4613@gmail.com>
2023-04-02 23:13:06 +1200

    Image imported

--- components/ListingDetail.jsx
+++ components/ListingDetail.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useStateContext } from "../context/StateContext";
+import Image from "next/image";
 
 const ListingDetail = ({ data: data, category: category }) => {
   const router = useRouter();
