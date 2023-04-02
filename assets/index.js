25bb91befd9b195b207dc51768b6b215cba9d39e
박지호 <yoho4613@gmail.com>
2023-04-03 01:28:49 +1200

    avatar updated

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,8 @@
 import mainLogo from './trademe_logo_new.png'
 import kiwi from "./kiwi.jpg"
 import landing from './landing-1.jpg'
+import logo from './logo.png'
+import avatar from './avatar.avif'
 
 
-export default { mainLogo, kiwi, landing }
+export default { mainLogo, kiwi, landing, logo, avatar }
